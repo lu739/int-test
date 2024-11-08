3d48aecd29a47b06f714e89b75ad8f8f2b813980
lu739 <kroshkalu739@gmail.com>
2024-11-08 23:56:23 +0300

    fix int

--- app/Http/Controllers/AmoController.php
+++ app/Http/Controllers/AmoController.php
@@ -9,7 +9,13 @@ class AmoController extends Controller
 {
     public function getLeads()
     {
-        dd(env('AMO_CRM_HOST'));
+        dd(
+            env('AMO_CRM_HOST'),
+            env('CLIENT_ID'),
+            env('CLIENT_SECRET'),
+            env('REDIS_CLIENT'),
+            env('DB_CONNECTION'),
+        );
         $data = Http::withHeaders([
             'Authorization' => 'Bearer ' . env('AMO_CRM_TOKEN')
         ])->get(env('AMO_CRM_HOST').'api/v4/leads');
